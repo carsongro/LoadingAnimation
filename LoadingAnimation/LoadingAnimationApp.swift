43ce66e89ccd31fb1ed867a44a51799b2d573af2
Carson Gross <carsongro@gmail.com>
2023-12-21 08:39:23 -0700

    Initial Commit

---
+++ LoadingAnimation/LoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAnimationApp.swift
+//  LoadingAnimation
+//
+//  Created by Carson Gross on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
